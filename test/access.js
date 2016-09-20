562d1cf5a6afe3865687b032c0e98e33aaf0f234
Sobieslaw Hrehorowicz <sobieslaw.hrehorowicz@xstream.net>
2016-09-20 19:42:42 +0200

    Slack, DB services

--- test/access.js
+++ test/access.js
@@ -7,6 +7,11 @@ const mochaPlugin = require('serverless-mocha-plugin');
 const wrapper     = mochaPlugin.lambdaWrapper;
 const expect      = mochaPlugin.chai.expect;
 
+const redis  = require("redis");
+const slack  = require("slack");
+const db = require('../lib/db');
+const sinon = require('sinon');
+
 const liveFunction = {
   region: process.env.SERVERLESS_REGION,
   lambdaFunction: process.env.SERVERLESS_PROJECT + '-access'
@@ -22,9 +27,17 @@ describe('access', () => {
     done()
   })
   
-  it('implement tests here', (done) => {
-    wrapper.run({body: {code: 'test'}}, (err, response) => {
+  it('should save token', (done) => {
+    let redisSpy = sinon.spy();
+    let slackSpy = sinon.stub(slack.oauth, 'access')
+
+    db.client = {set: redisSpy};
+    slackSpy.callsArgWith(1, null, 'testToken123');
+
+    wrapper.run({body: {code: 'code1223'}}, (err, response) => {
       expect(response).to.include.keys('ok');
+      expect(redisSpy.getCall(0).args[0]).to.equal('access-token');
+      expect(redisSpy.getCall(0).args[1]).to.equal('testToken123');
       done();
     });
   });
