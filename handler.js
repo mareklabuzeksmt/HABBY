ffd9be92b9246596707b6a7ef885ac59eaec4862
Sobieslaw Hrehorowicz <sobieslaw.hrehorowicz@xstream.net>
2016-09-26 21:44:44 +0200

    Refactor: Process message event

--- handler.js
+++ handler.js
@@ -1,10 +1,11 @@
 'use strict';
 
-const config = require('dotenv').config();
-const db = require('./lib/db');
-const slack = require('./lib/slack');
-const byeRegexp = /bye-bye|bye|bb|wylogowuję się|kończę/g;
-const reports = require('./lib/report');
+const config = require('dotenv').config()
+const db = require('./lib/db')
+const slack = require('./lib/slack')
+const byeRegexp = /bye-bye|bye|bb|wylogowuję się|kończę/g
+const reports = require('./lib/report')
+const async = require('async')
 
 // init
 db.connect();
@@ -37,30 +38,91 @@ module.exports.access = (event, context, cb) => {
 }
 
 module.exports.challenge = (event, context, cb) => {
-  console.log('event', JSON.stringify(event, null, 2));
+  console.log('event', JSON.stringify(event, null, 2))
   
   event.body = event.body || {}
 
   let slackEvent = 'event' in event.body ? event.body.event : null
 
-  let processMessageEvent = (slackEvent) => {
-     db.getUserCheckIn(slackEvent.user).then((checkIn) => {
-          if (checkIn == false) {
-            console.log(slackEvent.user + ' check in today');
-            db.setUserCheckIn(slackEvent.user);
-            db.setUserCheckInTime(slackEvent.user, slackEvent.ts);
-          } else if (byeRegexp.test(slackEvent.text)) {
-            db.getUserCheckOut(slackEvent.user).then((checkOut) => {
-              if (checkOut == false) {
-                console.log(slackEvent.user + ' check out today');
-                db.setUserCheckOut(slackEvent.user);
-                db.setUserCheckOutTime(slackEvent.user, slackEvent.ts);
-              }
-            });
+  console.log('slack event', slackEvent)
 
+  let processMessageEvent = (slackEvent) => {
+    let promise = new Promise((resolve, reject) => {
+      
+      async.waterfall([
+        (callback) => {
+          console.log('processing checking #1')
+          db.getUserCheckIn(slackEvent.user)
+          .then((checkIn) => {
+            callback(null, checkIn)  
+          })
+          .catch((checkIn) => {
+            callback('Error: checkin status could not be fetched')  
+          })
+        },
+        (checkIn, callback) => {
+          console.log('processing checking #2')
+
+          if (checkIn == false) {
+            console.log(slackEvent.user + ' check in today')
+
+            Promise.all([
+              db.setUserCheckIn(slackEvent.user),
+              db.setUserCheckInTime(slackEvent.user, slackEvent.ts)
+            ])
+            .then(() => {
+              callback(null, true)
+            })
+            .catch((checkIn) => {
+              callback('Error: checkin status could not be set for user')  
+            })            
+          } else {          
+            callback(null, true)
           }
-        });
+        },
+        (status, callback) => {
+            console.log('processing checking #3')
+            //
+            if (byeRegexp.test(slackEvent.text)) {
+              db.getUserCheckOut(slackEvent.user)
+              .then((checkOut) => {
+                if (checkOut == false) {
+                  console.log(slackEvent.user + ' check out today')
+
+                  Promise.all([
+                    db.setUserCheckOut(slackEvent.user),
+                    db.setUserCheckOutTime(slackEvent.user, slackEvent.ts)
+                  ])
+                  .then(() => {
+                    callback(null, true)
+                  })
+                  .catch((checkIn) => {
+                    callback('Error: checkout status could not be set for user')  
+                  })  
+                } else {
+                  callback(null, true)
+                }
+              })
+              .catch(() => {
+                callback('Error: checkout status could not be set for user')  
+              })
+            } else {
+                  callback(null, true)
+            } 
+        }
+      ], (err, result) => {
+          console.log(4)
+          if (err) {
+            reject(err)
+          } else {
+            resolve('ok')
+          }
+      })
+    })      
+    
+    return promise
   }
+  
 
   let processReportMessage = (userReports, teamReports, slackEvent) => {
     let queryParams, p1, p2, reportPeriod;
@@ -92,26 +154,41 @@ module.exports.challenge = (event, context, cb) => {
 
   if ('challenge' in event.body) {
     console.log('challenge')
-     context.succeed({ challenge: event.body.challenge })
+    context.succeed({ challenge: event.body.challenge })
   } else if (slackEvent && slackEvent.type === 'message') {
     console.log('message')
-    db.getStatusChannel().then((channel) => {
+
+    db.getStatusChannel()
+    .then((channel) => {
       //check if message on status channel
       if (channel === slackEvent.channel) {
-        console.log('new message on status channel');
-        processMessageEvent(slackEvent);
+        console.log('new message on status channel')
+        
+        processMessageEvent(slackEvent)
+        .then(() => {
+           context.succeed({status: 'ok'})
+        })
+        .catch((err) => {
+          console.log(err)
+          context.fail('Error process event failed.' + err.message)
+        }) 
+        
       } else {
         //check if message on direct channel and process /report query
         db.getDirectChannels().then((channels) => {
           if(channels.length && channels.indexOf(slackEvent.channel) > -1) {
             console.log('new message on direct channel');
             //processMessageEvent(slackEvent);
+            
             let userReportMatches = slackEvent.text.match(reports.userReportRegexp);
             let teamReportMatches = slackEvent.text.match(reports.teamReportRegexp);
             processReportMessage(userReportMatches,teamReportMatches,slackEvent);
           }
         })
       }
+    })
+    .catch(() => {
+      context.fail('Error status channel could not be fetched')
     }) 
   } else {
     context.fail('Unsupported operation')
